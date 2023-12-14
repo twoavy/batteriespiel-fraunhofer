b3132f83880746670146ce7a88dc1d2d7e9861f2
Manuel Herr <mh@2av.de>
2023-12-14 13:10:45 +0100

    added pois

--- Assets/Scripts/3D-Viewer/RotateModel.cs
+++ Assets/Scripts/3D-Viewer/RotateModel.cs
@@ -1,9 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateModel : MonoBehaviour
 {
+    public float mouseRotationSpeed = 10f;
+    public float touchRotationSpeed = 0.5f;
+    
+    public Boolean rotateXAxis = true;
+    public Boolean rotateYAxis = false;
+    
+    public Camera cam;
+    private void OnMouseDrag()
+    {
+        float rotationX = Input.GetAxis("Mouse X") * mouseRotationSpeed;
+        float rotationY = Input.GetAxis("Mouse Y") * mouseRotationSpeed;
+        
+        Vector3 right = Vector3.Cross(cam.transform.up, transform.position - cam.transform.position);
+        Vector3 up = Vector3.Cross(transform.position - cam.transform.position, right);
+
+        if (rotateYAxis)
+        {
+            transform.rotation = Quaternion.AngleAxis(-rotationX, up) * transform.rotation;
+        }
+
+        if (rotateXAxis)
+        {
+            transform.rotation = Quaternion.AngleAxis(-rotationY, right) * transform.rotation;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
